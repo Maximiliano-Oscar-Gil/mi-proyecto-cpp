c8fb920004479d642f16d556c702b3d7a367979d
Maximiliano Oscar Gil <maximiliano.gil11@hotmail.com>
2025-08-22 08:27:44 -0300

    Agregada función sumar y ejemplo de uso

--- main.cpp
+++ main.cpp
@@ -4,3 +4,16 @@ int main() {
     std::cout << "¡Hola desde C++!" << std::endl;
     return 0;
 }
+
+#include <iostream>
+
+// Función nueva que suma dos números
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    std::cout << "¡Hola desde C++!" << std::endl;
+    std::cout << "La suma de 3 + 5 es: " << sumar(3, 5) << std::endl;
+    return 0;
+}
